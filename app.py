d07661b1543f5d9013f33a39d676eda5b19bf616
Annnyan <novikovaannna@gmail.com>
2022-12-25 18:08:11 +0200

    Add error handling

--- app.py
+++ app.py
@@ -8,7 +8,8 @@ from resources.signup import blp as SignupBlueprint
 from resources.login import blp as LoginBlueprint
 from resources.category import blp as CategoryBlueprint
 from resources.record import blp as RecordBlueprint
-from flask import Flask
+from flask import Flask, jsonify
+
 
 def create_app():
     app = Flask(__name__)
@@ -28,6 +29,34 @@ def create_app():
     api = Api(app)
 
     jwt = JWTManager(app)
+
+    @jwt.expired_token_loader
+    def expired_token_callback(jwt_header, jwt_payload):
+        return (
+            jsonify({"message": "The token has expired.", "error": "token_expired"}),
+            401,
+        )
+
+    @jwt.invalid_token_loader
+    def invalid_token_callback(error):
+        return (
+            jsonify(
+                {"message": "Signature verification failed.", "error": "invalid_token"}
+            ),
+            401,
+        )
+
+    @jwt.unauthorized_loader
+    def missing_token_callback(error):
+        return (
+            jsonify(
+                {
+                    "description": "Request does not contain an access token.",
+                    "error": "authorization_required",
+                }
+            ),
+            401,
+        )
 
     with app.app_context():
         db.create_all()
